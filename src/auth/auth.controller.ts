e5cfb50989d6371bc713fb4b4f089550a8d4ecba
saifakib <mdsaifakib@gmail.com>
2024-03-23 15:16:29 +0600

    swagger implementation

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,16 +1,20 @@
-import { Body, Controller, HttpCode, Post } from '@nestjs/common';
+import { Body, Controller, HttpCode, Post, UsePipes } from '@nestjs/common';
 import { AuthService } from './auth.service';
 import { ZodPipe } from 'src/zod-validation/zod-validation.pipe';
-import { SignUpDto, SignUpSchema, LoginSchema, LoginDto } from './dto';
-import { RefreshTokenDto, RefreshTokenSchema } from './dto/refreshToken-dto';
+import { SignUpDto, signUpSchema, loginSchema, LoginDto, RefreshTokenDto, refreshTokenSchema } from './dto';
+import {  } from './dto/refreshToken-dto';
+import { ApiTags } from '@nestjs/swagger';
+import { ZodValidationPipe } from '@anatine/zod-nestjs';
 
+@ApiTags('Auth')
+@UsePipes(ZodValidationPipe)
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
   @HttpCode(201)
-  signup(@Body(new ZodPipe(SignUpSchema)) signupDto: SignUpDto) {
+  signup(@Body(new ZodPipe(signUpSchema)) signupDto: SignUpDto) {
     try {
       return this.authService.signup(signupDto);
     } catch (err: any) {
@@ -20,7 +24,7 @@ export class AuthController {
 
   @Post('login')
   @HttpCode(200)
-  login(@Body(new ZodPipe(LoginSchema)) loginDto: LoginDto) {
+  login(@Body(new ZodPipe(loginSchema)) loginDto: LoginDto) {
     try {
       return this.authService.login(loginDto);
     } catch (err) {
@@ -30,7 +34,7 @@ export class AuthController {
 
   @Post('refreshToken')
   @HttpCode(201)
-  refreshToken(@Body(new ZodPipe(RefreshTokenSchema)) refreshToken: RefreshTokenDto) {
+  refreshToken(@Body(new ZodPipe(refreshTokenSchema)) refreshToken: RefreshTokenDto) {
     try {
       return this.authService.refreshToken(refreshToken);
     } catch (err) {
--- src/main.ts
+++ src/main.ts
@@ -1,14 +1,23 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ZodFilter } from './zod-validation/zod-validation.filter';
+import { SwaggerModule } from '@nestjs/swagger'
+import { swaggerConfig } from './config/swagger.config';
+import { Logger } from '@nestjs/common';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
   const globalPrefix = 'api/v1';
   app.setGlobalPrefix(globalPrefix);
+
+  // Swagger
+  const swaggerDocument = SwaggerModule.createDocument(app, swaggerConfig);
+  SwaggerModule.setup(`${globalPrefix}/docs`, app, swaggerDocument);
   
   app.useGlobalFilters(new ZodFilter());
-  await app.listen(3000);
+  await app.listen(3000, () => {
+    Logger.log('Listening at http://localhost:' + 3000 + '/' + globalPrefix);
+  });
 }
 bootstrap();
