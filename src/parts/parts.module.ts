c8bfbb28b0516a41dbb379eee209f1c1ee2407d6
saifakib <mdsaifakib@gmail.com>
2024-05-15 23:04:57 +0600

    Models module include

--- src/parts/parts.module.ts
+++ src/parts/parts.module.ts
@@ -5,10 +5,13 @@ import { CategoryRepository } from '@/parts/category/category.repository';
 import { BrandsController } from '@/parts/brands/brands.controller';
 import { BrandsService } from '@/parts/brands/brands.services';
 import { BrandsRepository } from '@/parts/brands/brands.repository';
+import { ModelsController } from '@/parts/models/models.controller';
+import { ModelsService } from '@/parts/models/models.service';
+import { ModelsRepository } from '@/parts/models/models.repository';
 
 @Module({
   imports: [],
-  controllers: [CategoryController, BrandsController],
-  providers: [CategoryService, CategoryRepository, BrandsService, BrandsRepository],
+  controllers: [CategoryController, BrandsController, ModelsController],
+  providers: [CategoryService, CategoryRepository, BrandsService, BrandsRepository, ModelsService, ModelsRepository],
 })
 export class PartsModule {}
