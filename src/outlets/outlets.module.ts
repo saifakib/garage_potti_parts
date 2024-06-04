b3c674343e08949bede70451b06bd7a0b90141d7
saifakib <mdsaifakib@gmail.com>
2024-06-04 23:22:32 +0600

    include outlet modules

--- src/outlets/outlets.module.ts
+++ src/outlets/outlets.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
+import { OutletController } from '@/outlets/outlets/outlets.controller';
+import { OutletService } from '@/outlets/outlets/outlets.service';
+import { OutletRepository } from '@/outlets/outlets/outlet.repository';
 
 @Module({
   imports: [],
-  controllers: [],
-  providers: [],
+  controllers: [OutletController],
+  providers: [OutletService, OutletRepository],
 })
 export class OutletsModule {}
